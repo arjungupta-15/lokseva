998fe54f8d8bb78f0a164de63c5cd460d149e745
Arya Padole <aryapadole523@gmail.com>
2025-12-30 00:24:11 +0530

    map

---
+++ lokseva/app/(tabs)/map.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Map: React.FC = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Hello World 🌍</h1>
+      <p>This is map.tsx file</p>
+    </div>
+  );
+};
+
+export default Map;
